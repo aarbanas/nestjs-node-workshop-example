aa6b235163a59cb6f34952fef050810943ccbfd5
Ivan Brcko <ivan.brcko@porsche.digital>
2026-05-19 14:21:29 +0200

    feat: update TypeScript configuration, add Docker Compose, and enhance main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+export async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Node workshop')
     .setDescription('CRUD for cars and manufacturers')
     .setVersion('1.0')
+    .addServer(process.env.API_BASE_URL ?? '/')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customCssUrl: 'https://unpkg.com/swagger-ui-dist@5/swagger-ui.css',
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js',
+    ],
+  });
+
+  return app;
+}
+
+async function bootstrapApp() {
+  const app = await bootstrap();
 
   await app.listen(3000);
 }
 
-bootstrap();
+bootstrapApp();
